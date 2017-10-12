05ff5d91fafe240068e9063236ccd1e881a76ef2
Ahmad Nasikin <ahmad.nasikin09@gmail.com>
2017-10-12 16:14:10 +0700

    setup routing dan register

---
+++ client/src/components/Register.vue
@@ -0,0 +1,37 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      regis: {
+        username: '',
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    register () {
+      let self = this
+      this.$http.post('/register', {
+        username: self.regis.username,
+        email: self.regis.email,
+        password: self.regis.password
+      })
+      .then(response => {
+        this.$router.push('/')
+      })
+      .catch(err => {
+        console.err(err)
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+import Register from '@/components/Register'
+import Login from '@/components/Login'
+import Articles from '@/components/Article'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    },
+    {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/articles',
+      name: 'Articles',
+      component: Articles
     }
   ]
 })
