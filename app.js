6e6af5ff8cb2eef667593b399006d19921901bae
Ahmad Nasikin <ahmad.nasikin09@gmail.com>
2017-10-12 13:23:40 +0700

    setting awal

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const logger = require('morgan')
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://livecoding:livecoding@cluster0-shard-00-00-mtaex.mongodb.net:27017,cluster0-shard-00-01-mtaex.mongodb.net:27017,cluster0-shard-00-02-mtaex.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin')
+
+var app = express()
+
+app.use(cors())
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+var index = require('./routers/index')
+
+app.use('/', index)
+
+app.listen(process.env.PORT || 3000)
