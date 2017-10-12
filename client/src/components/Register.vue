9ba558ad0d1df418ff639941c2278c3ecad1d1ae
Ahmad Nasikin <ahmad.nasikin09@gmail.com>
2017-10-12 16:20:16 +0700

    udah bisa register

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,5 +1,44 @@
 <template>
-  
+  <div>
+    <div class="">
+      <div class="container">
+      <div class="text-header">
+      </div>
+        <div class="row">
+          <div class="formRegister">
+            <div class="col-md-6 col-md-offset-3">
+              <form @submit.prevent="register()"class="form-horizontal" id="login">
+                <div class="form-group">
+                  <label for="inputPassword" class="col-lg-2 control-label">Username</label>
+                  <div class="col-lg-10">
+                    <input type="text" class="form-control" placeholder="username" v-model="regis.username">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="inputPassword" class="col-lg-2 control-label">Email</label>
+                  <div class="col-lg-10">
+                    <input type="text" class="form-control" placeholder="email" v-model="regis.email">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="inputPassword" class="col-lg-2 control-label">Password</label>
+                  <div class="col-lg-10">
+                    <input type="password" class="form-control" placeholder="Password" v-model="regis.password">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <div class="col-lg-10 col-lg-offset-2">
+                    <button type="reset" class="btn btn-default">Cancel</button>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -22,6 +61,7 @@ export default {
         password: self.regis.password
       })
       .then(response => {
+        console.log('responya', response)
         this.$router.push('/')
       })
       .catch(err => {
