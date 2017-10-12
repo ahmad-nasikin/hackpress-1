0c4cdc3fcbf4b2fefd86857ba937308f3e1d2bcf
Ahmad Nasikin <ahmad.nasikin09@gmail.com>
2017-10-12 16:53:12 +0700

    udah buat method nya, tinggal masukin datanya

--- client/src/components/Article.vue
+++ client/src/components/Article.vue
@@ -1,13 +1,75 @@
 <template>
-  
+  <div>
+    <div>
+      <div >
+        <form method="post" @submit.prevent="addArticle()">
+          <input type="text" name="todo" placeholder="input todo " v-model="post.title">
+          <input type="text" name="todo" placeholder="input todo " v-model="post.content">
+          <input type="text" name="todo" placeholder="input todo " v-model="post.category">
+          <button name="button">add article</button>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      articles: [],
+      post: {
+        title: '',
+        content: '',
+        category: ''
+      }
+    }
+  },
+  methods: {
+    getArticle () {
+      this.$http.get('/articles', {
+      })
+      .then(result => {
+        console.log('result', result.data)
+        this.articles = result.data
+      })
+      .catch(err => {
+        console.error(err)
+      })
+    },
+    addArticle () {
+      this.$http.post('/articles', {
+        title: this.post.title,
+        content: this.post.content,
+        category: this.post.category
+      }, {
+        headers: {
+          token: localStorage.getItem('token')
+        }
+      })
+      .then(result => {
+        this.articles.push(result.data)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+    },
+    // deleteArticle (data, index) {
+    //   this.$http.delete(`/article/${data}`, {
+    //     headers: {
+    //       token: localStorage.getItem('token')
+    //     }
+    //   })
+    //   .then(response => {
+    //     self.articles.splice(index, 1)
+    //   })
+    // },
+    logout () {
+      localStorage.clear()
+    }
+  },
+  created () {
+    this.getArticle()
+  }
 }
 </script>
-
-<style>
-
-</style>
