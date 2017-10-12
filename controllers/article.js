929310f6cc9356ae5e9bd942af8497dc113f516f
Ahmad Nasikin <ahmad.nasikin09@gmail.com>
2017-10-12 15:22:20 +0700

    udah dapat si author waktu create article

--- controllers/article.js
+++ controllers/article.js
@@ -2,14 +2,15 @@ const models = require('../models/Article')
 const jwt = require('jsonwebtoken')
 
 var create = (req, res) => {
-  console.log('ini header', req.headers.token)
+  console.log('ini header ...', req.headers.auth)
   models.create({
     title: req.body.title,
     content: req.body.content,
     category: req.body.category,
-    author: req.headers.auth_id
+    author: req.headers.auth._id
   })
   .then((result) => {
+    console.log('author', result.author)
     console.log('ini result', result)
     res.send(result)
   })
